4f5386fa1a57f6e433c0925b66bc1a390203f347
André Spadetto Leão <andre.sp.leao@gmail.com>
2016-05-14 19:07:20 -0300

    Metodo de cadastrar um projeto.

--- js/controllers/projetoCtrl.js
+++ js/controllers/projetoCtrl.js
@@ -3,8 +3,16 @@ angular.module("ipsProject").controller("projetoCtrl", function($scope,projetosA
     $scope.ferramentas= ferramentas.data;
     $scope.pessoas=pessoas.data;     
 
+    $scope.cadastrarProjeto = function(projeto){            
+            projetosAPI.postCadastrarProjeto(projeto).success(function(data){
+               delete $scope.projetos;
+                $scope.formCadastroProjeto.$setPristine();
+                $location.path("/projeto");
+            }); 
+     }       
+
     $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year
-    format: 'dd-mm-yyyy' });
-});
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year
+        format: 'dd-mm-yyyy' });
+    });
